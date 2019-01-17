7cff89d3b14c90bc0b8b9a872ec37e2d318896bc
Ewles <40330977@live.napier.ac.uk>
2019-01-17 11:57:58 +0000

    more pong

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include <string>
 
 using namespace sf;
 using namespace std;
 
 Vector2f ballVelocity;
 bool server = false;
+void reset();
 
 const Keyboard::Key controls[4] = {
 	Keyboard::A,   // Player1 UP
@@ -21,6 +23,12 @@ const float paddleSpeed = 400.f;
 CircleShape ball;
 RectangleShape paddles[2];
 
+Font font;
+Text text;
+int score1 = 0;
+int score2 = 0;
+
+
 void Load() {
 	// Set size and origin of paddles
 	for (auto &p : paddles) {
@@ -36,13 +44,19 @@ void Load() {
 	// reset Ball Position
 	ball.setPosition(gameWidth/2, gameHeight/2);
 	ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
-
-	/*font.loadFromFile("res/XPED_Expanded.ttf");
+	
+	font.loadFromFile("C:/Users/40330977/Desktop/Games_Engineering_1/res/Xeron.ttf");
 	// Set text element to use font
 	text.setFont(font);
 	// set the character size to 24 pixels
-	text.setCharacterSize(24);*/
+	text.setCharacterSize(24);
+	auto s1 = std::to_string(score1);
+	auto s2 = std::to_string(score2);
+	text.setString(s1 + " || " + s2);
+	text.setPosition((gameWidth * .5f) - (text.getLocalBounds().width * .5f), 0);
 }
+
+
 
 void Update(RenderWindow &window) {
 	// Reset clock, recalculate deltatime
@@ -99,11 +113,13 @@ void Update(RenderWindow &window) {
 	else if (bx > gameWidth) {
 		// right wall
 		//reset();
-//		reset();
+		score1++;
+		reset();
 	}
 	else if (bx < 0) {
 		// left wall
-//		reset();
+		score2++;
+		reset();
 	}
 	else if (
 		//ball is inline or behind paddle
@@ -136,20 +152,26 @@ void Render(RenderWindow &window) {
 	window.draw(paddles[0]);
 	window.draw(paddles[1]);
 	window.draw(ball);
+	window.draw(text);
 }
 
 void reset() {
 	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-	paddles[1].setPosition( paddleSize.x / 2, gameHeight / 2);
+	paddles[1].setPosition(gameWidth - paddleSize.x / 2, gameHeight / 2);
 	// reset Ball Position
 	ball.setPosition(gameWidth / 2, gameHeight / 2);
 	ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
 
-//	// Update Score Text
-//	text.setString(...);
-//	// Keep Score Text Centered
-//	text.setPosition((gameWidth * .5f) - (text.getLocalBounds().width * .5f), 0);
+	//	// Update Score Text
+	auto s1 = std::to_string(score1);
+	auto s2 = std::to_string(score2);
+	text.setString(s1 + " || " + s2);
+	//	// Keep Score Text Centered
+		text.setPosition((gameWidth * .5f) - (text.getLocalBounds().width * .5f), 0);
+		//text.setPosition(gameWidth / 2, gameHeight / 2);
 }
+
+
 
 int main() {
 	RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
