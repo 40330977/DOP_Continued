2d5ae8f1aee91e8768da6a894554f676346c53a7
Ewles <40330977@live.napier.ac.uk>
2019-01-24 12:01:46 +0000

    SI progress

--- practical_2/Ship.h
+++ practical_2/Ship.h
@@ -15,3 +15,12 @@ public:
 	//Update, virtual so can be overrided, but not pure virtual
 	virtual void Update(const float &dt);
 };
+
+class Invader : public Ship {
+public:
+	static bool direction;
+	static float speed;
+	Invader(sf::IntRect ir, sf::Vector2f pos);
+	Invader();
+	void Update(const float &dt) override;
+};
--- practical_2/main.cpp
+++ practical_2/main.cpp
@@ -5,14 +5,19 @@
 using namespace sf;
 using namespace std;
 
+
 sf::Texture spritesheet;
 sf::Sprite invader;
 std::vector<Ship *> ships;
+
 
 //void Load();
 
 const int gameWidth = 800;
 const int gameHeight = 600;
+int invaders_rows;
+int invaders_columns;
+//float r1, c1;
 
 void Load() {
 	if (!spritesheet.loadFromFile("C:/Users/40330977/Desktop/Games_Engineering_1/res/invaders_sheet.png")) {
@@ -20,6 +25,20 @@ void Load() {
 	}
 	invader.setTexture(spritesheet);
 	invader.setTextureRect(sf::IntRect(0, 0, 32, 32));
+
+	/*Invader* inv = new Invader(sf::IntRect(0, 0, 32, 32), { 100,100 });
+	ships.push_back(inv);*/
+
+	for (int r = 0; r < invaders_rows; ++r) {
+		auto rect = IntRect(0, 0, 32, 32);
+		for (int c = 0; c < invaders_columns; ++c) {
+			/*r1 = r;
+			c1 = c;*/
+			Vector2f position = Vector2f(r+100, c+100);
+			auto inv = new Invader(rect, position);
+			ships.push_back(inv);
+		}
+	}
 }
 
 void Update(RenderWindow &window) {
@@ -39,11 +58,18 @@ void Update(RenderWindow &window) {
 	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 		window.close();
 	}
+
+	for (auto &s : ships) {
+		s->Update(dt);
+	}
 }
 
 void Render(RenderWindow &window) {
 	// Draw Everything
-	window.draw(invader);
+	//window.draw(invader);
+	for (const auto s : ships) {
+		window.draw(*s);
+	}
 }
 
 
