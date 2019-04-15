0b3706ef557f2e12bb965ef7edf163976b276197
RoddyE <roderickewles@gmail.com>
2019-04-15 15:00:00 +0100

    ai reset work

--- practical_6_platformer/components/cmp_enemy_ai.cpp
+++ practical_6_platformer/components/cmp_enemy_ai.cpp
@@ -1,4 +1,6 @@
 #include "cmp_enemy_ai.h"
+#include "engine.h"
+#include <LevelSystem.h>
 
 using namespace std;
 using namespace sf;
@@ -21,14 +23,24 @@ void EnemyAIComponent::update(double dt) {
   if (_direction.y<0 && gsim>1000.0f)
   {
 	  _direction.y *= -1.f;
+	  gsim = 0;
   }
   move(_direction * (float)(dt * _speed));
   ActorMovementComponent::update(dt);
   gsim++;
+  const auto pos = _parent->getPosition();
+  //Teleport to start if we fall off map.
+  if (pos.y > ls::getHeight() * ls::getTileSize()) {
+	  _parent->setPosition(Vector2f(0.0f, 0.0f));
+  }
 }
 
-EnemyAIComponent::EnemyAIComponent(Entity* p) : ActorMovementComponent(p) {
-  _direction = Vector2f(1.0f, 0);
+EnemyAIComponent::EnemyAIComponent(Entity* p) : ActorMovementComponent(p), _player(_parent->scene->ents.find("player")[0]) {
+	auto pl = _player.lock();
+	//Vector2f chaser = pl->getPosition() - _parent->getPosition();
+	 //length(chaser);
+ // _direction = Vector2f(chaser.x , 1.0f);
+	//_direction = Vector2f(1.0f, 1.0f);
   _jumper = Vector2f(1.0f, 0);
   _speed = 100.0f;
 }
--- practical_6_platformer/components/cmp_enemy_ai.h
+++ practical_6_platformer/components/cmp_enemy_ai.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include "cmp_actor_movement.h"
+#include <Box2D/Dynamics/b2Body.h>
 
 class EnemyAIComponent : public ActorMovementComponent {
 protected:
   sf::Vector2f _direction;
   sf::Vector2f _jumper;
+  std::weak_ptr<Entity> _player;
+  b2Body* _body;
 public:
 	bool isGrounded() const;
 	void update(double dt) override;
 
   explicit EnemyAIComponent(Entity* p);
+
+
 
   EnemyAIComponent() = delete;
 };
